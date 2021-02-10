efe674d34b7d5fea24225aa4ea7a7e52283119d3
John T. Wodder II <git@varonathe.org>
2021-02-10 16:55:51 -0500

    Got tests to pass

--- src/iterpath/__init__.py
+++ src/iterpath/__init__.py
@@ -13,29 +13,39 @@ __url__          = 'https://github.com/jwodder/iterpath'
 from   operator import attrgetter
 import os
 from   pathlib  import Path
-from   typing   import Iterator, NamedTuple
+from   typing   import Iterator, NamedTuple, Union
 
 class DirEntries(NamedTuple):
     dirpath: Path
     entries: Iterator[os.DirEntry]
 
-def iterpath(dirpath, topdown=True, include_root=False, dirs=True, sort=False):
-    def get_entries(p):
-        entries = os.scandir(p)
+def iterpath(
+    dirpath: Union[str, os.PathLike],
+    topdown: bool = True,
+    include_root: bool = False,
+    dirs: bool = True,
+    sort: bool = False,
+) -> Iterator[Path]:
+
+    def get_entries(p: Union[str, os.PathLike]) -> DirEntries:
+        entries: Iterator[os.DirEntry] = os.scandir(p)
         if sort:
             entries = iter(sorted(entries, key=attrgetter("name")))
         return DirEntries(Path(p), entries)
+
     dirstack = [get_entries(dirpath)]
+    if include_root and topdown:
+        yield dirstack[0].dirpath
     while dirstack:
         try:
             e = next(dirstack[-1].entries)
         except StopIteration:
-            if dirs and not topdown and (len(dirstack) > 1 or include_root):
-                yield dirstack[-1].dirpath
-            dirstack.pop()
+            d = dirstack.pop()
+            if dirs and not topdown and (dirstack or include_root):
+                yield d.dirpath
             continue
         if e.is_dir():
-            if dirs and topdown and (len(dirstack) > 1 or include_root):
+            if dirs and topdown:
                 yield Path(e)
             dirstack.append(get_entries(e))
         else:
